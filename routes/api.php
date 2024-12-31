a250e941cc6f15150672815eb35e156a2dcebddc
cristian <cchura.cpc@gmail.com>
2024-12-30 22:39:28 -0400

    migraciones y modelos

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UsuarioController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -20,3 +21,6 @@ Route::prefix('/v1/auth')->group(function(){
     });
 
 });
+
+// CRUD de Usuarios
+Route::apiResource("user", UsuarioController::class);
